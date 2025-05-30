c493c0cfde5f8f2ea8ee162c1e4b63ed513f9f6b
jasjnr <54113139+jasjnr@users.noreply.github.com>
2025-05-30 12:42:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const basicAuth = require('basic-auth');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
